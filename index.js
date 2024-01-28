d790a14a9ec3ebccddb48a9539127ea5dcf81998
Bakhtimes <85776705+Bakhtimes@users.noreply.github.com>
2024-01-28 21:42:25 +0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,61 @@
+
+
+
+
+const express = require('express')
+const axios = require('axios')
+const port = 5555;
+
+const app = express();
+const bodyParser = require('body-parser');
+app.use( bodyParser.json() );  
+app.use(bodyParser.urlencoded({ extended: true }));
+const https = require("https");
+const { error } = require('console');
+
+
+
+const apiKey = '9e1ad7b033ff33905c3e7b861e8b432b'
+const city = 'Nur-Sultan'
+const api = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;
+
+app.set('view engine', 'ejs');
+app.post('/', function(req,res) {
+    console.log(req.body.geolocation)
+            var city = req.body.city;
+    
+    const address = req.query.address;
+    var url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`
+    axios.get(url).then(resp => {
+        const data = resp.data;
+        console.log(data)
+        const city = data.name;
+        const lat = data.coord.lat;
+        const lon = data.coord.lon;
+        const temp = data.main.temp;
+        const sunsetTime = new Date(data.sys.sunset * 1000).toLocaleTimeString()
+        res.render('index', {data: data, latitude:lat, longitude:lon, city:city, data:data, temp:temp, sunsetTime:sunsetTime})
+    }).catch(err => {
+        res.send('Invalid city name')
+    })
+    // var city = req.body.city;
+    
+    // const address = req.query.address;
+    // const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`
+    // axios.get(url).then(resp => {
+    //     const data = resp.data;
+    //     const city = data.name;
+    //     const lat = data.coord.lat;
+    //     const lon = data.coord.lon;
+    //     const temp = data.main.temp;
+    //     const sunsetTime = new Date(data.sys.sunset * 1000).toLocaleTimeString()
+    //     res.render('index', {data: data, latitude:lat, longitude:lon, city:city, data:data, temp:temp, sunsetTime:sunsetTime})
+    // }).catch(err => {
+    //     res.send('Invalid city name')
+    // })
+})
+
+app.get('/', (req, res) => {
+    res.render('search')
+})
+app.listen(port, () => console.log(`Server started on port ${port}.`))
---
+++ script.js
@@ -0,0 +1,6 @@
+let map;
+function initMap() {
+    map = new google.maps.Map(document.getElementById('map'), {
+        center: {}
+    })
+}
